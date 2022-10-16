1e5deabe8efb05ba6683821dbcc3c9dcd221cfe2
Flojea <98753660+Flojea@users.noreply.github.com>
2022-10-16 23:16:16 +0200

    Add files via upload

---
+++ Fichiers utiles/twitter.js
@@ -0,0 +1,4 @@
+<a class="twitter-timeline" href="https://twitter.com/pssGuy/timelines/524678699061641216" 
+data-widget-id="524686407298596864">Soccer</a>
+
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)    [0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
